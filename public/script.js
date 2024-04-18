c4f4e04b5a71fc81200c83ecd357836d2458aeb0
Demetrius Mack <demetriusomack00@gmail.com>
2024-04-18 15:04:10 -0400

    server.js

--- public/script.js
+++ public/script.js
@@ -15,35 +15,57 @@ const showCrafts = async() => {
         craftsDiv.innerHTML = "Sorry, no crafts";
         return;
     }
+    const crafts = await getCrafts();
+        const column01 = document.getElementById("column01");
+        const column02 = document.getElementById("column02");
+        const column03 = document.getElementById("column03");
+        const column04 = document.getElementById("column04");
 
-    //now loop through the json
-    craftsJSON.forEach((craft)=>{
-        const section = document.createElement("section");
-        section.className ="w3-col l3 m6 w3-margin-bottom";
+        for(int i=0; i< crafts.length(); i++){
+          const section = document.createElement("section");
 
-        const divCard = document.createElement("div");
-        divCard.className = "w3-display-container w3-border";
-        section.appendChild(divCard);
+          const title = document.createElement("h3");
+          title.innerHTML= crafts[i].name;
 
-        const img = document.createElement("img");
-        img.src = "http://localhost:3000/api/crafts/images";
-        img.style= "width:100%";
-        divCard.appendChild(img);
+          const img = document.createElement("img");
+          img.src = crafts[i].image;
 
-        const divOverlay = document.createElement("div");
-        divCard.className = "w3-display-middle w3-display-hover w3-xlarge w3-text-white";
-        divCard.appendChild(divOverlay);
+          section.append(title);
+          section.append(img);
 
-        const name = document.createElement("p");
-        name.textContent = craft.name;
-        divOverlay.appendChild(name);
-
-        divCard.onclick = () => {
-          alert("Description: " +craft.description);
+          `column{i%4}`.append(section);
         };
 
-        craftsDiv.appendChild(section);
-    });
+    // // now loop through the json
+    // craftsJSON.forEach((craft)=>{
+    //     const section = document.createElement("section");
+    //     section.className ="w3-col l3 m6 w3-margin-bottom";
+
+    //     const divCard = document.createElement("div");
+    //     divCard.className = "w3-display-container w3-border";
+    //     section.appendChild(divCard);
+
+    //     const img = document.createElement("img");
+    //     img.src = craft.image;
+    //     img.style= "width:100%";
+    //     divCard.appendChild(img);
+
+    //     const divOverlay = document.createElement("div");
+    //     divCard.className = "w3-display-middle w3-display-hover w3-xlarge w3-text-white";
+    //     divCard.appendChild(divOverlay);
+
+    //     const name = document.createElement("p");
+    //     name.textContent = craft.name;
+    //     divOverlay.appendChild(name);
+
+    //     divCard.onclick = () => {
+    //       alert("Description: " +craft.description);
+    //     };
+
+    //     craftsDiv.appendChild(section);
+        
+        
+    // });
   };
 
 showCrafts();
